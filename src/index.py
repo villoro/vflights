18b5100e93ff0019819963d315419de5d7ba2474
Villoro <villoro7@gmail.com>
2020-01-22 20:28:03 +0100

    [NEW] basic plot with real data

--- src/index.py
+++ src/index.py
@@ -3,25 +3,24 @@
 """
 
 import dash
+import plotly.express as px
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+
+from data_loader import DFG
+
+df = DFG[(DFG["Origin"] == "BCN") & (DFG["Destination"] == "CAG")]
+df = df.sort_values("Inserted", ascending=False).drop_duplicates(["Date"])
+
 
 APP = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 APP.layout = html.Div(
     children=[
         dbc.NavbarSimple(brand="VFlights", brand_href="#", color="primary", dark=True),
-        dcc.Graph(
-            id="plot_prices",
-            figure={
-                "data": [
-                    {"x": [1, 2, 3], "y": [4, 1, 2], "type": "bar", "name": "SF"},
-                    {"x": [1, 2, 3], "y": [2, 4, 5], "type": "bar", "name": "Montréal"},
-                ],
-                "layout": {"title": "Dash Data Visualization"},
-            },
-        ),
+        dcc.Graph(id="plot_prices", figure=px.bar(df, x="Date", y="Price")),
         dbc.Row(
             [
                 dbc.Col(
@@ -41,6 +40,7 @@ APP.layout = html.Div(
         ),
     ]
 )
+
 
 if __name__ == "__main__":
     APP.run_server(debug=True)
