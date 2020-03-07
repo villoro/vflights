eef60597ab22f7073137bcb4b303d4577dc4390b
Villoro <villoro7@gmail.com>
2020-03-07 12:48:46 +0100

    [NEW] modal in layout optimized

--- src/layout.py
+++ src/layout.py
@@ -8,12 +8,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 
+import plots
 from data_loader import DFG
-
-df = DFG[(DFG["Origin"] == "BCN") & (DFG["Destination"] == "CAG")]
-df = df.sort_values("Inserted", ascending=False).drop_duplicates(["Date"])
-
-airports = DFG["Origin"].unique()
 
 
 def get_options(iterable):
@@ -23,37 +19,63 @@ def get_options(iterable):
     return [{"label": x, "value": x} for x in iterable]
 
 
+def get_filters():
+    """ Filters in the model callable from the nav bar """
+
+    airports = DFG["Origin"].unique().tolist()
+    carriers = DFG["Carrier"].unique().tolist()
+
+    # Use a dict with title and element
+    filters = {
+        "Origin": dcc.Dropdown(id="origin", options=get_options(airports), value="CAG"),
+        "Destination": dcc.Dropdown(id="dest", options=get_options(airports), value="BCN"),
+        "Direct flights": dcc.RadioItems(
+            id="direct",
+            options=[
+                {"label": "Direct", "value": "1"},
+                {"label": "All", "value": "0"},
+                {"label": "No direct", "value": "-1"},
+            ],
+            value="0",
+            inputStyle={"margin-left": "20px"},
+        ),
+        "Carriers": dcc.Dropdown(
+            id="carriers", options=get_options(carriers), value=carriers, multi=True
+        ),
+        "Show flights from past": dcc.RadioItems(
+            id="past",
+            options=[{"label": "Yes", "value": "1"}, {"label": "No", "value": "0"}],
+            value="0",
+            inputStyle={"margin-left": "20px"},
+        ),
+    }
+
+    # Concat all titles, elements and spacers
+    modal_body = []
+    for title, element in filters.items():
+        modal_body += [html.H4(title), element, html.Br()]
+
+    # Return the whole model info
+    return [
+        dbc.ModalHeader("Filters"),
+        dbc.ModalBody(modal_body),
+        dbc.ModalFooter(dbc.Button("Close", id="b_close", className="ml-auto")),
+    ]
+
+
 def get_layout():
     """ Creates the dash layout """
 
     return html.Div(
         children=[
             dbc.NavbarSimple(
-                [
-                    dbc.Button("Filter", id="b_open"),
-                    dbc.Modal(
-                        [
-                            dbc.ModalHeader("Filters"),
-                            dbc.ModalBody(
-                                [
-                                    html.H4("Origin"),
-                                    dcc.Dropdown(options=get_options(airports), value="CAG"),
-                                    html.Br(),
-                                    html.H4("Destination"),
-                                    dcc.Dropdown(options=get_options(airports), value="BCN"),
-                                ]
-                            ),
-                            dbc.ModalFooter(dbc.Button("Close", id="b_close", className="ml-auto")),
-                        ],
-                        id="modal",
-                    ),
-                ],
+                [dbc.Button("Filter", id="b_open"), dbc.Modal(get_filters(), id="modal")],
                 brand="VFlights",
                 brand_href="#",
                 color="primary",
                 dark=True,
             ),
-            dcc.Graph(id="plot_prices", figure=px.bar(df, x="Date", y="Price")),
+            dcc.Graph(id="plot_prices"),
             dbc.Row(
                 [
                     dbc.Col(
